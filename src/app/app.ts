f2413718e59074a2b29734eef12b1d9136035bfc
Sophie Large <sophie.large@wanadaoo.fr>
2025-11-18 15:12:16 +0100

    fix(initialization)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Footpage } from './footpage/footpage';
+import { Footer } from './footer/footer';
 import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     Header,
-    Footpage
+    Footer
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
