8f5715842b290835b6ac3123dedb6ee85c8e5dc3
paullemeilleur <134516970+paullemeilleur@users.noreply.github.com>
2025-11-20 17:25:03 +0100

    feat(footer): implement footer component with tabs and content

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.css',
+  styleUrls: ['./footer.css'],
 })
 export class Footer {
+  activeTab: 'remerciements' | 'contacts' = 'remerciements';
 
+  constructor() {}
 }
