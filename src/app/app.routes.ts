6befb1bf62eecace3b4884f74593ddd34ea90276
sophielrge <sophie.large@wanadoo.fr>
2025-12-11 11:37:04 +0100

    feat(account)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from "./login/login";
 import { DetectionComponent } from "./detection/detection";
 import { PlanningComponent } from "./planning/planning";
 import { HomepageComponent } from "./homepage/homepage";
+import {AccountComponent} from './account/account';
 
 export const routes: Routes = [{ path: '', redirectTo: 'homepage', pathMatch: 'full' },
                                 { path: 'detection', component: DetectionComponent},
                                  { path: 'planning', component: PlanningComponent },
                                  { path: 'login', component: LoginComponent },
-                                 { path: 'homepage', component: HomepageComponent }];
+                                 { path: 'homepage', component: HomepageComponent },
+                                { path: 'account', component: AccountComponent }];
