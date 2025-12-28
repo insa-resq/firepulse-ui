598c95506ba2cdceb7a60142a4b1c7b4438d7ffd
Paul Lemeilleur <paul.lemeilleur@gmail.com>
2025-12-28 16:32:43 +0100

    fix(map): remove default icon

--- src/app/detection/map/map.ts
+++ src/app/detection/map/map.ts
@@ -27,12 +27,8 @@ export class Map implements AfterViewInit, OnChanges {
     this.L = await import('leaflet');
 
     // Fix pour les icônes Leaflet avec Vite/Angular
-    delete (this.L.Icon.Default.prototype as any)._getIconUrl;
-    this.L.Icon.Default.mergeOptions({
-      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-    });
+    // delete (this.L.Icon.Default.prototype as any)._getIconUrl;
+    // this.L.Icon.Default.mergeOptions({});
 
     // Initialisations Leaflet
     this.map = this.L.map('alert-map', { maxBounds: [[41, -5], [51.5, 9]], maxBoundsViscosity: 1.0 }).setView([46.8, 2.2], 6);
