880b41656283454e55d2557c32d4531de2e65b62
sophielrge <sophie.large@wanadoo.fr>
2026-01-05 11:01:33 +0100

    fix(planning): correct current week

--- src/app/planning/planning.ts
+++ src/app/planning/planning.ts
@@ -16,6 +16,8 @@ export class PlanningComponent implements OnInit {
   hasGlobalPlanningRights = false;
   currentWeek!: number;
 
+  currentDate = new Date();
+
   constructor(
     private userService: UserService,
   ) {}
@@ -24,19 +26,27 @@ export class PlanningComponent implements OnInit {
     this.hasGlobalPlanningRights =
       this.userService.hasRight('GLOBAL_PLANNING');
 
-    this.currentWeek = this.getCurrentWeek();
+    this.currentWeek = this.getCurrentWeek(this.currentDate) ;
+    console.log(this.currentWeek);
   }
 
-  getCurrentWeek(): number {
-    const date = new Date();
+  getCurrentWeek(date : Date): number {
     const startYear = new Date(date.getFullYear(), 0, 1);
     const diff = Math.floor(
       (date.getTime() - startYear.getTime()) / 86400000
     );
-    return Math.ceil((diff + startYear.getDay() + 1) / 7);
+    return Math.ceil((diff + startYear.getDay() + 1) / 7) - 1;
   }
 
+  previousWeek() {
+    this.currentDate.setDate(this.currentDate.getDate() - 7);
+    this.currentWeek = this.getCurrentWeek(this.currentDate);
+  }
 
+  nextWeek() {
+    this.currentDate.setDate(this.currentDate.getDate() + 7);
+    this.currentWeek = this.getCurrentWeek(this.currentDate);
+  }
 
 
 
